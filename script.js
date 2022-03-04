b4bb6b8210dec64c63d4161be3dfae746f1c215d
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-04 23:05:47 +0300

    Added possiblilty for a number to have a decimal place

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.op');
 const clear = document.querySelector('.clear');
 const backSpace = document.querySelector('.backspace');
+const decimal = document.querySelector('.decimal');
+const equal = document.querySelector('.equals');
 const expressionField = document.querySelector('.expression');
 const answerField = document.querySelector('.answer');
 
-let array = []; //Values will be stored for operation
-let iterate = 0;
+let array = [];
+let num1 = 0, num2 = 0;
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        array.push(number.innerText);
+        console.log(array)
+    })
+})
+
+decimal.addEventListener('click', () => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == '.') {
+            return
+        }
+    }
+
+    array.push(decimal.innerText);
+    
+})
 

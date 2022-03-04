0a6c214d27639551182218b8394ec3035aa9f030
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-05 00:19:06 +0300

    Computation of two number is achieved.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.op');
-const clear = document.querySelector('.clear');
-const backSpace = document.querySelector('.backspace');
+const clearButton = document.querySelector('.clear');
+const backSpaceButton = document.querySelector('.backspace');
 const decimal = document.querySelector('.decimal');
-const equal = document.querySelector('.equals');
+const equalButton = document.querySelector('.equals');
 const expressionField = document.querySelector('.expression');
 const answerField = document.querySelector('.answer');
 
 let array = [];
-let num1 = '', num2 = '';
+let num1 = '', num2 = '', temp = '';
 let op = '';
+let result = 0;
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
@@ -26,6 +27,7 @@ decimal.addEventListener('click', () => {
             return
         }
     }
+    console.log(array, num1, op)
     array.push(decimal.innerText);
     
 })
@@ -37,9 +39,10 @@ operators.forEach(operator => {
         if (array.length == 0 || array[length-1] == operator) {
             return
         }
-        if (num1 == 0) {
+        if (num1 == '') {
             num1 = array.splice(0, array.length);
             op = operator.innerText;
+            console.log(array, num1, op)
         }
         
     })
@@ -47,15 +50,16 @@ operators.forEach(operator => {
 
 //Clear button functionality
 
-clear.addEventListener('click', () => {
+clearButton.addEventListener('click', () => {
     array.splice(0, array.length)
     num1 = '', num2 = '';
     op = '';
+    console.log(array, num1, op)
 })
 
 //Backspace button functionality
 
-backSpace.addEventListener('click', () => {
+backSpaceButton.addEventListener('click', () => {
     if (array.length != 0) {
         array.pop();
     }
@@ -69,7 +73,48 @@ backSpace.addEventListener('click', () => {
     else if (op != '') {
         array.pop();
     }
+    console.log(array, num1, op)
 })
 
+//Equal button functionality (Do the desired computation)
 
+equalButton.addEventListener('click', () => {
 
+    if (array.length == 0) {
+        return
+    }
+
+    num2 = array.splice(0, array.length);
+    console.log(num1, num2);
+
+    for (let i = 0; i < num1.length; i++) {
+        temp += num1[i];
+    }
+    num1 = '';
+    num1 = temp;
+    temp = ''
+
+    for (let i = 0; i < num2.length; i++) {
+        temp += (num2[i]);
+    }
+    
+    num2 = '';
+    num2 = temp;
+    temp = '';
+
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    if (op == '+') {
+        result = num1 + num2;
+    }
+    else if (op == '-') {
+        result = num1 - num2;
+    }
+    else if (op == '/') {
+        result = num1/num2;
+    }
+    else result = num1 * num2;
+
+    console.log(result)
+
+})

e06f6a3d139f78c4d755f2b38fdb4cf5c1984382
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-04 21:47:39 +0300

    Targeted all the elements in DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.op');
+const clear = document.querySelector('.clear');
+const backSpace = document.querySelector('.backspace');
+const expressionField = document.querySelector('.expression');
+const answerField = document.querySelector('.answer');
+
+let array = []; //Values will be stored for operation
+let iterate = 0;
+
